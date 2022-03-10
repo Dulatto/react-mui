789579ac8c084ffcf826a2c0d7ecaa90342059dd
Dulatto <dulat.omarov@gmail.com>
2022-03-09 23:18:53 -0500

    mui radio

--- src/components/MuiRadioButton.tsx
+++ src/components/MuiRadioButton.tsx
@@ -1,4 +1,4 @@
-import {Box, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio} from '@mui/material'
+import {Box, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio, FormHelperText} from '@mui/material'
 import {useState} from 'react'
 
 export const MuiRadioButton = () => {
@@ -10,7 +10,7 @@ export const MuiRadioButton = () => {
 
   return (
     <Box>
-        <FormControl>
+        <FormControl error>
             <FormLabel id='job-experience-group-label'>
                 Years of experience
             </FormLabel>
@@ -19,11 +19,13 @@ export const MuiRadioButton = () => {
               aria-labelledby='job-experience-group-label'
               value={value}
               onChange={handleChange}
+              row
               >
-                <FormControlLabel  control={<Radio />} label='0-2' value='0-2'/>
+                <FormControlLabel  control={<Radio size='small' color='secondary'/>} label='0-2' value='0-2'/>
                 <FormControlLabel  control={<Radio />} label='3-5' value='3-5'/>
                 <FormControlLabel  control={<Radio />} label='6-10' value='6-10'/>
             </RadioGroup>
+            <FormHelperText>Invalid selection</FormHelperText>
         </FormControl>
     </Box>
   )
