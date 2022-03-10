7001f8f14e7396f04e0c9010975de7923f49a499
Dulatto <dulat.omarov@gmail.com>
2022-03-09 22:36:40 -0500

    mui radio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
-import { MuiSelect } from './components/MuiSelect';
+import { MuiRadioButton } from './components/MuiRadioButton';
+//import { MuiSelect } from './components/MuiSelect';
 //import { MuiButton } from './components/MuiButton';
 //import { MuiTextField } from './components/MuiTextField';
 //import { MuiTypography } from './components/MuiTypography';
@@ -9,8 +10,9 @@ function App() {
     <div className="App">
      {/* <MuiTypography /> 
      <MuiButton/>
-     <MuiTextField />*/}
-     <MuiSelect />
+     <MuiTextField />
+     <MuiSelect />*/}
+     <MuiRadioButton />
     </div>
   );
 }
--- src/components/MuiRadioButton.tsx
+++ src/components/MuiRadioButton.tsx
@@ -2,7 +2,18 @@ import {Box, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio} from '
 
 export const MuiRadioButton = () => {
   return (
-    <Box>MuiRadioButton</Box>
+    <Box>
+        <FormControl>
+            <FormLabel id='job-experience-group-label'>
+                Years of experience
+            </FormLabel>
+            <RadioGroup name='job-experience-group' aria-labelledby='job-experience-group-label'>
+                <FormControlLabel  control={<Radio />} label='0-2' value='0-2'/>
+                <FormControlLabel  control={<Radio />} label='3-5' value='3-5'/>
+                <FormControlLabel  control={<Radio />} label='6-10' value='6-10'/>
+            </RadioGroup>
+        </FormControl>
+    </Box>
   )
 }
 
