751c08b9c0645e9512ffcf0edc313ef7df3cbeb7
Dulatto <dulat.omarov@gmail.com>
2022-03-09 22:28:13 -0500

    mui radio

---
+++ src/components/MuiRadioButton.tsx
@@ -0,0 +1,8 @@
+import {Box, FormControl, FormLabel, FormControlLabel, RadioGroup, Radio} from '@mui/material'
+
+export const MuiRadioButton = () => {
+  return (
+    <Box>MuiRadioButton</Box>
+  )
+}
+
