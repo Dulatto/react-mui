bcd7f6032194efb92c947188087e267ca1cff40d
Dulatto <dulat.omarov@gmail.com>
2022-03-10 22:28:59 -0500

    mui rating

--- src/components/MuiRating.tsx
+++ src/components/MuiRating.tsx
@@ -1,9 +1,32 @@
 import {Stack, Rating} from '@mui/material'
+import {useState} from 'react';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 
 export const MuiRating = () => {
+    const [value, setValue] = useState<number | null>(null)
+  console.log({value})
+    const handleChange =(_event: React.ChangeEvent<{}>, newValue: number | null)=>{
+        setValue(newValue)
+    }
   return (
     <Stack spacing={2}>
-      <Rating />
+      <Rating 
+         value={value} 
+         onChange={handleChange} 
+         precision={0.5} 
+         size='large'
+         icon={<FavoriteIcon fontSize='inherit' color='error'/>}
+         emptyIcon={<FavoriteBorderIcon fontSize='inherit'/>}        
+         />
+          <Rating 
+         value={value} 
+         onChange={handleChange} 
+         precision={0.5} 
+         size='large'
+         icon={<FavoriteIcon fontSize='inherit' color='error'/>}
+         emptyIcon={<FavoriteBorderIcon fontSize='inherit'/>}        
+         highlightSelectedOnly/>
     </Stack>
   )
 }
