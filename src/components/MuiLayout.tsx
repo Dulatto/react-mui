7ed36737b26f2337ffd0f61e37259bf67135d6ec
Dulatto <dulat.omarov@gmail.com>
2022-03-14 22:34:59 -0400

    mui grid

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -25,11 +25,11 @@ export const MuiLayout = () => {
 
     </Box>
     </Stack>
-    <Grid container my={4}>
-        <Grid item> <Box bgcolor='primary.light' p={2}>Item 1</Box></Grid>
-        <Grid item><Box bgcolor='primary.light' p={2}>Item 2</Box></Grid>
-        <Grid item><Box bgcolor='primary.light' p={2}>Item 3</Box></Grid>
-        <Grid item><Box bgcolor='primary.light' p={2}>Item 4</Box></Grid>
+    <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+        <Grid item xs={6}> <Box bgcolor='primary.light' p={2}>Item 1</Box></Grid>
+        <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 2</Box></Grid>
+        <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 3</Box></Grid>
+        <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 4</Box></Grid>
     </Grid>
     </>
   )
