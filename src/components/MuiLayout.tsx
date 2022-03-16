c439fe666af156a506d7db693b27f4fafd41cddc
Dulatto <dulat.omarov@gmail.com>
2022-03-15 22:38:46 -0400

    mui paper

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,10 +1,10 @@
 
-import {Box, Stack, Divider, Grid} from '@mui/material'
+import {Box, Stack, Divider, Grid, Paper} from '@mui/material'
 
 
 export const MuiLayout = () => {
   return (
-      <>
+      <Paper sx={{padding: '32px'}} elevation={4}>
       <Stack 
       sx={{border:'1px solid'}} 
       direction='row' 
@@ -31,6 +31,6 @@ export const MuiLayout = () => {
         <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 3</Box></Grid>
         <Grid item xs={6}><Box bgcolor='primary.light' p={2}>Item 4</Box></Grid>
     </Grid>
-    </>
+    </Paper>
   )
 }
