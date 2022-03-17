f68dae4fc402447665876fb10e8a28c35675e71b
Dulatto <dulat.omarov@gmail.com>
2022-03-16 22:49:38 -0400

    mui accordion

--- src/components/MuiAccordion.tsx
+++ src/components/MuiAccordion.tsx
@@ -1,10 +1,15 @@
 import {Accordion, AccordionSummary, AccordionDetails, Typography}  from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import {useState} from 'react';
 
 export const MuiAccordion = () => {
+    const [expanded, setExpanded] = useState<string | false>(false)
+ const handleChange =(isExpanded:boolean, panel:string)=> {
+     setExpanded(isExpanded ? panel : false)
+ }
   return (
     <div>
-        <Accordion>
+        <Accordion expanded={expanded === 'panel1'} onChange={(event, isExpanded) => handleChange(isExpanded, 'panel1')}>
             <AccordionSummary 
             id='panel1-header'
             aria-controls='panel1-content'
@@ -21,7 +26,7 @@ export const MuiAccordion = () => {
             </AccordionDetails>
         </Accordion>
 
-        <Accordion>
+        <Accordion expanded={expanded === 'panel2'}  onChange={(event, isExpanded) => handleChange(isExpanded, 'panel2')}>
             <AccordionSummary 
             id='panel2-header'
             aria-controls='panel2-content'
@@ -38,7 +43,7 @@ export const MuiAccordion = () => {
             </AccordionDetails>
         </Accordion>
 
-        <Accordion>
+        <Accordion expanded={expanded === 'panel3'} onChange={(event, isExpanded) => handleChange(isExpanded, 'panel3')}>
             <AccordionSummary 
             id='panel3-header'
             aria-controls='panel3-content'
