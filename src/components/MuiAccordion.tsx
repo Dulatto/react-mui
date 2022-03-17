e7e6e1d5255060c21538f9d1dad679593a8b5a49
Dulatto <dulat.omarov@gmail.com>
2022-03-16 22:35:17 -0400

    mui accordion

--- src/components/MuiAccordion.tsx
+++ src/components/MuiAccordion.tsx
@@ -20,6 +20,40 @@ export const MuiAccordion = () => {
                </Typography>
             </AccordionDetails>
         </Accordion>
+
+        <Accordion>
+            <AccordionSummary 
+            id='panel2-header'
+            aria-controls='panel2-content'
+            expandIcon={<ExpandMoreIcon />}
+            >
+               <Typography>Accordion 2</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+               <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                 Accusantium quos modi molestias odit qui laborum ipsam amet.
+                  Expedita vitae, velit veniam nemo iusto provident exercitationem!
+                   Adipisci quas numquam dolores praesentium.
+               </Typography>
+            </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+            <AccordionSummary 
+            id='panel3-header'
+            aria-controls='panel3-content'
+            expandIcon={<ExpandMoreIcon />}
+            >
+               <Typography>Accordion 3</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+               <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                 Accusantium quos modi molestias odit qui laborum ipsam amet.
+                  Expedita vitae, velit veniam nemo iusto provident exercitationem!
+                   Adipisci quas numquam dolores praesentium.
+               </Typography>
+            </AccordionDetails>
+        </Accordion>
     </div>
   )
 }
