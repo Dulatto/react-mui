2f81759c2412fa1749af7d0f580126feb9ff977c
Dulatto <dulat.omarov@gmail.com>
2022-03-23 21:45:24 -0400

    mui breadcrumbs

--- src/components/MuiBreadcrumbs.tsx
+++ src/components/MuiBreadcrumbs.tsx
@@ -1,9 +1,34 @@
 import {Box, Breadcrumbs, Link, Typography}  from '@mui/material'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 
 export const MuiBreadcrumbs = () => {
   return (
     <Box m={2}>
         <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" href='#'>Home</Link>
+            <Link underline="hover" href='#'>Catalog</Link>
+            <Link underline="hover" href='#'>Accessories</Link>
+            <Typography color='text.primary'>Shoes</Typography>
+        </Breadcrumbs>
+        <Breadcrumbs aria-label="breadcrumb" separator="-">
+            <Link underline="hover" href='#'>Home</Link>
+            <Link underline="hover" href='#'>Catalog</Link>
+            <Link underline="hover" href='#'>Accessories</Link>
+            <Typography color='text.primary'>Shoes</Typography>
+        </Breadcrumbs>
+        <Breadcrumbs aria-label="breadcrumb" separator={<NavigateNextIcon fontSize="small"/>}>
+            <Link underline="hover" href='#'>Home</Link>
+            <Link underline="hover" href='#'>Catalog</Link>
+            <Link underline="hover" href='#'>Accessories</Link>
+            <Typography color='text.primary'>Shoes</Typography>
+        </Breadcrumbs>
+        <Breadcrumbs 
+           aria-label="breadcrumb" 
+           separator={<NavigateNextIcon fontSize="small"/>}
+           maxItems={2}
+           //itemsBeforeCollapse={2}
+          //</Box> itemsAfterCollapse={2}
+          >
             <Link underline="hover" href='#'>Home</Link>
             <Link underline="hover" href='#'>Catalog</Link>
             <Link underline="hover" href='#'>Accessories</Link>
