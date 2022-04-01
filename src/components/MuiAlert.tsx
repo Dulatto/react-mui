5c5b018bb63b0cf65afd99810243346984cbf35c
Dulatto <dulat.omarov@gmail.com>
2022-03-31 22:23:57 -0400

    mui alert

--- src/components/MuiAlert.tsx
+++ src/components/MuiAlert.tsx
@@ -1,4 +1,5 @@
-import {Stack, Alert} from '@mui/material'
+import {Stack, Alert, AlertTitle, Button} from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check'
 
 export const MuiAlert = () => {
   return (
@@ -7,6 +8,36 @@ export const MuiAlert = () => {
       <Alert severity="warning">Warning alert</Alert>
       <Alert severity="info">Info alert</Alert>
       <Alert severity="success">Success alert</Alert>
+
+      <Alert variant="outlined" severity="error">Error alert</Alert>
+      <Alert variant="outlined" severity="warning">Warning alert</Alert>
+      <Alert variant="outlined" severity="info">Info alert</Alert>
+      <Alert variant="outlined" severity="success">Success alert</Alert>
+
+      <Alert variant='filled' severity="error" onClose={()=> alert('Close alert')}>
+          <AlertTitle>Error</AlertTitle>
+          Error alert
+          </Alert>
+      <Alert variant='filled' severity="warning">
+      <AlertTitle>Warning</AlertTitle>
+          Warning alert
+          </Alert>
+      <Alert variant='filled' severity="info">
+      <AlertTitle>Info</AlertTitle>
+          Info alert
+          </Alert>
+      <Alert 
+      variant='filled' 
+      severity="success" 
+      icon={<CheckIcon fontSize='inherit'/>}
+      action={
+          <Button color='inherit' size='small'>
+              UNDO
+          </Button>
+      }>
+      <AlertTitle>Success</AlertTitle>
+          Success alert
+          </Alert>
     </Stack>
   )
 }
