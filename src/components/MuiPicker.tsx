2afdc180de555f7d418253bd9bf9ce07d1b59511
Dulatto <dulat.omarov@gmail.com>
2022-04-04 22:55:23 -0400

    mui datepicker

--- src/components/MuiPicker.tsx
+++ src/components/MuiPicker.tsx
@@ -1,9 +1,11 @@
 import {Stack, TextField} from '@mui/material'
-import { DatePicker } from '@mui/lab'
+import { DatePicker, TimePicker, DateTimePicker } from '@mui/lab'
 import {useState} from 'react'
 
 export const MuiPicker = () => {
     const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+    const [selectedTime, setSelectedTime] = useState<Date | null>(null)
+    const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(null)
   return (
     <Stack spacing={4} sx={{width: '250px'}}>
        <DatePicker 
@@ -12,6 +14,22 @@ export const MuiPicker = () => {
           value={selectedDate} 
           onChange={(newValue)=>{ 
               setSelectedDate(newValue)
+          }}/>
+
+    <TimePicker 
+          label="Time Picker" 
+          renderInput={(param)=> <TextField {...param}/>}
+          value={selectedTime} 
+          onChange={(newValue)=>{ 
+              setSelectedTime(newValue)
+          }}/>
+
+    <DateTimePicker 
+          label="Date Time Picker" 
+          renderInput={(param)=> <TextField {...param}/>}
+          value={selectedDateTime} 
+          onChange={(newValue)=>{ 
+              setSelectedDateTime(newValue)
           }}/>
     </Stack>
   )
